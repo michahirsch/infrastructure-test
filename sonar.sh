a463e68296a6c45f731cd6976680cd57c7204b55
Kai Zimmermann <kaizimmerm@posteo.net>
2015-11-09 09:12:25 +0100

    reduced man command

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+mvn sonar:sonar \
   -Dmaven.test.failure.ignore=true \
   -Dclirr=true \
   -Dsonar.analysis.mode=issues \
