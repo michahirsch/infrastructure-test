10f05d87f8701c14bdb5b8996e0d12a1e62443f9
Kai Zimmermann <kaizimmerm@posteo.net>
2015-11-09 15:29:04 +0100

    Script magic

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,9 @@
+
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B -e -V \
   -Dmaven.test.failure.ignore=true \
   -Dclirr=true \
   -Dsonar.analysis.mode=issues \
-  -Dsonar.github.pullRequest=$CI_PULL_REQUESTS \
+  -Dsonar.github.pullRequest=`echo $CI_PULL_REQUESTS| awk -F'/' '{print $7}'` \
   -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
   -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH
 #  -Dsonar.login=$SONAR_LOGIN \
