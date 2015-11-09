338373a768578f935f0a281eb7a7e7a441282cca
Kai Zimmermann <kaizimmerm@posteo.net>
2015-11-09 14:38:54 +0100

    Remove sonar credentials

--- sonar.sh
+++ sonar.sh
@@ -6,6 +6,6 @@ mvn sonar:sonar \
   -Dsonar.github.repository=$CIRCLE_PROJECT_REPONAME \
   -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
   -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-  -Dsonar.host.url=$SONAR_HOST_URL \
-  -Dsonar.login=$SONAR_LOGIN \
-  -Dsonar.password=$SONAR_PASSWORD
+  -Dsonar.host.url=$SONAR_HOST_URL #\
+#  -Dsonar.login=$SONAR_LOGIN \
+#  -Dsonar.password=$SONAR_PASSWORD
