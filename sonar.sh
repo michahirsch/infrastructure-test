9dc0576cc90b52760a033fb0eef3ff45f6280bbf
Kai Zimmermann <kaizimmerm@posteo.net>
2015-11-09 15:12:22 +0100

    next try

--- sonar.sh
+++ sonar.sh
@@ -2,7 +2,7 @@ mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B -e 
   -Dmaven.test.failure.ignore=true \
   -Dclirr=true \
   -Dsonar.analysis.mode=issues \
-  -Dsonar.github.pullRequest=$CIRCLE_PR_NUMBER \
+  -Dsonar.github.pullRequest=$CI_PULL_REQUESTS \
   -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
   -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH
 #  -Dsonar.login=$SONAR_LOGIN \
